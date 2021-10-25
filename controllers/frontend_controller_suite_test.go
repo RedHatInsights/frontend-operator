f96400008987b96d8fbd679e7d8bbc157bb5e214
Pete Savage <psavage@redhat.com>
2021-10-25 15:53:02 +0100

    Removed comments

--- controllers/frontend_controller_suite_test.go
+++ controllers/frontend_controller_suite_test.go
@@ -16,16 +16,8 @@ import (
 
 // +kubebuilder:docs-gen:collapse=Imports
 
-/*
-The first step to writing a simple integration test is to actually create an instance of CronJob you can run tests against.
-Note that to create a CronJob, you’ll need to create a stub CronJob struct that contains your CronJob’s specifications.
-Note that when we create a stub CronJob, the CronJob also needs stubs of its required downstream objects.
-Without the stubbed Job template spec and the Pod template spec below, the Kubernetes API will not be able to
-create the CronJob.
-*/
 var _ = Describe("Frontend controller", func() {
 
-	// Define utility constants for object names and testing timeouts/durations and intervals.
 	const (
 		FrontendName      = "test-frontend"
 		FrontendNamespace = "default"
@@ -103,22 +95,12 @@ var _ = Describe("Frontend controller", func() {
 			}
 			Expect(k8sClient.Create(ctx, &frontendEnvironment)).Should(Succeed())
 
-			/*
-				After creating this CronJob, let's check that the CronJob's Spec fields match what we passed in.
-				Note that, because the k8s apiserver may not have finished creating a CronJob after our `Create()` call from earlier, we will use Gomega’s Eventually() testing function instead of Expect() to give the apiserver an opportunity to finish creating our CronJob.
-				`Eventually()` will repeatedly run the function provided as an argument every interval seconds until
-				(a) the function’s output matches what’s expected in the subsequent `Should()` call, or
-				(b) the number of attempts * interval period exceed the provided timeout value.
-				In the examples below, timeout and interval are Go Duration values of our choosing.
-			*/
-
 			deploymentLookupKey := types.NamespacedName{Name: frontend.Name, Namespace: FrontendNamespace}
 			ingressLookupKey := types.NamespacedName{Name: frontend.Name, Namespace: FrontendNamespace}
 			configMapLookupKey := types.NamespacedName{Name: frontendEnvironment.Name, Namespace: FrontendNamespace}
 
 			createdDeployment := &apps.Deployment{}
 
-			// We'll need to retry getting this newly created CronJob, given that creation may not immediately happen.
 			Eventually(func() bool {
 				err := k8sClient.Get(ctx, deploymentLookupKey, createdDeployment)
 				if err != nil {
@@ -126,11 +108,9 @@ var _ = Describe("Frontend controller", func() {
 				}
 				return true
 			}, timeout, interval).Should(BeTrue())
-			// Let's make sure our Schedule string value was properly converted/handled.
 			Expect(createdDeployment.Name).Should(Equal(FrontendName))
 
 			createdIngress := &networking.Ingress{}
-			// We'll need to retry getting this newly created CronJob, given that creation may not immediately happen.
 			Eventually(func() bool {
 				err := k8sClient.Get(ctx, ingressLookupKey, createdIngress)
 				if err != nil {
@@ -138,7 +118,6 @@ var _ = Describe("Frontend controller", func() {
 				}
 				return true
 			}, timeout, interval).Should(BeTrue())
-			// Let's make sure our Schedule string value was properly converted/handled.
 			Expect(createdIngress.Name).Should(Equal(FrontendName))
 
 			createdConfigMap := &v1.ConfigMap{}
@@ -149,7 +128,6 @@ var _ = Describe("Frontend controller", func() {
 				}
 				return true
 			}, timeout, interval).Should(BeTrue())
-			// Let's make sure our Schedule string value was properly converted/handled.
 			Expect(createdConfigMap.Name).Should(Equal(FrontendEnvName))
 			Expect(createdConfigMap.Data).Should(Equal(map[string]string{
 				"fed-modules.json": "{\"test-frontend\":{\"manifestLocation\":\"/apps/inventory/fed-mods.json\",\"modules\":[{\"id\":\"test\",\"module\":\"./RootApp\",\"routes\":[{\"pathname\":\"/test/href\"}]}]}}",
