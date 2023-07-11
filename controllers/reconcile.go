c95cde5edfdd06f6837a5b15bf3b10ae6c69cb72
Adam Drew <addrew@redhat.com>
2023-07-11 09:18:55 -0400

    Update limits and requests based on resource-limits in limit ranges on our cluster

--- controllers/reconcile.go
+++ controllers/reconcile.go
@@ -143,12 +143,12 @@ func populateContainer(d *apps.Deployment, frontend *crd.Frontend, frontendEnvir
 		VolumeMounts: populateContainerVolumeMounts(frontendEnvironment),
 		Resources: v1.ResourceRequirements{
 			Requests: v1.ResourceList{
-				v1.ResourceCPU:    resource.MustParse("1m"),
-				v1.ResourceMemory: resource.MustParse("128Mi"),
+				v1.ResourceCPU:    resource.MustParse("100m"),
+				v1.ResourceMemory: resource.MustParse("256Mi"),
 			},
 			Limits: v1.ResourceList{
-				v1.ResourceCPU:    resource.MustParse("10m"),
-				v1.ResourceMemory: resource.MustParse("256Mi"),
+				v1.ResourceCPU:    resource.MustParse("1"),
+				v1.ResourceMemory: resource.MustParse("512Mi"),
 			},
 		},
 	},
