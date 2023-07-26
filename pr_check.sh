36fcfde55ba09e6376c2be56623047bff801bc3c
Adam Drew <addrew@redhat.com>
2023-07-26 14:14:03 -0400

    I think this is a rough approximation of what clowder does.

--- pr_check.sh
+++ pr_check.sh
@@ -6,7 +6,13 @@ mkdir -p "$PWD/.docker"
 
 CONTAINER_NAME="${FEO_CONTAINER_NAME:-frontend-operator-pr-check-$ghprbPullId}"
 
-docker run -i --name $CONTAINER_NAME -v $PWD:/workspace:ro quay.io/bholifie/frontend-op-pr-check:v0.0.8 /workspace/build/pr_check_inner.sh
+# We're mounting the jenkins workspace over the root of the container
+# This means that the pr_check_inner.sh script will be run in the context of the jenkins workspace
+# This confused me for a while because pr_check_inner.sh is also copied into the pr check container at build time
+# but the template_check.sh isn't. I couldn't figure out how it was sourcing it
+#docker run -i --name $CONTAINER_NAME -v $PWD:/workspace:ro quay.io/bholifie/frontend-op-pr-check:v0.0.8 /workspace/build/pr_check_inner.sh
+
+docker build -t $CONTAINER_NAME -f build/Dockerfile.pr 
 
 TEST_RESULT=$?
 
