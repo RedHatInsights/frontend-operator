a2134106d8a09b396a072e33004085ad29fae293
Adam Drew <addrew@redhat.com>
2023-07-26 13:15:35 -0400

    Bump go in Dockerfile.pr. Slim down the pr check script to see if I can get a handle on it

--- pr_check.sh
+++ pr_check.sh
@@ -2,32 +2,18 @@
 
 set -exv
 
-# Note, this does not currently work with podman. pr_check_inner.sh has insufficient permissions
-RUNTIME="docker"
-DOCKER_CONF="$PWD/.docker"
-mkdir -p "$DOCKER_CONF"
-
-export IMAGE_TAG=`git rev-parse --short HEAD`
-export IMAGE_NAME=quay.io/cloudservices/frontend-operator
+mkdir -p "$PWD/.docker"
 
 CONTAINER_NAME="${FEO_CONTAINER_NAME:-frontend-operator-pr-check-$ghprbPullId}"
-# NOTE: Make sure this volume is mounted 'ro', otherwise Jenkins cannot clean up the workspace due to file permission errors
-set +e
-# Run the pr check container (stored in the build dir) and invoke the
-# pr_check_inner as its command
-$RUNTIME run -i \
---name $CONTAINER_NAME \
--v $PWD:/workspace:ro \
-quay.io/bholifie/frontend-op-pr-check:v0.0.8 \
-/workspace/build/pr_check_inner.sh
+
+docker run -i --name $CONTAINER_NAME -v $PWD:/workspace:ro quay.io/bholifie/frontend-op-pr-check:v0.0.8 /workspace/build/pr_check_inner.sh
 
 TEST_RESULT=$?
 
 mkdir -p artifacts
 
-$RUNTIME cp $CONTAINER_NAME:/container_workspace/artifacts/ $PWD
+docker cp $CONTAINER_NAME:/container_workspace/artifacts/ $PWD
 
-$RUNTIME rm -f $CONTAINER_NAME
-set -e
+docker rm -f $CONTAINER_NAME
 
 exit $TEST_RESULT
