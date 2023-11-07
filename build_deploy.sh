ee7481403f4faaa861a55796920ac7f94b67b9eb
Adam Drew <addrew@redhat.com>
2023-11-07 10:04:08 -0500

    This is utterly exhausting

--- build_deploy.sh
+++ build_deploy.sh
@@ -20,6 +20,9 @@ mkdir -p "$DOCKER_CONF"
 
 docker buildx inspect --builder multiarchbuilder --bootstrap
 
+docker buildx ls
+
+
 docker --config="$DOCKER_CONF" login -u="$QUAY_USER" -p="$QUAY_TOKEN" quay.io
 docker --config="$DOCKER_CONF" login -u="$RH_REGISTRY_USER" -p="$RH_REGISTRY_TOKEN" registry.redhat.io
 
