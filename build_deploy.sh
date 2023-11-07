3aaf82071727e9d0410fb3c7edac7dc725fecaf0
Adam Drew <addrew@redhat.com>
2023-11-07 10:08:21 -0500

    Retrigger base image rebuild?

--- build_deploy.sh
+++ build_deploy.sh
@@ -19,8 +19,6 @@ DOCKER_CONF="$PWD/.docker"
 mkdir -p "$DOCKER_CONF"
 
 docker buildx inspect --builder multiarchbuilder --bootstrap
-
-docker buildx ls
 docker buildx use multiarchbuilder
 
 docker --config="$DOCKER_CONF" login -u="$QUAY_USER" -p="$QUAY_TOKEN" quay.io
