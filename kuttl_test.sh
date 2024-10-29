bd354c8ae1ce36f9756ed6ae8b015fda07f5403d
Matt Knop <mknop@redhat.com>
2024-10-29 09:47:41 -0600

    install kubectl

--- kuttl_test.sh
+++ kuttl_test.sh
@@ -2,6 +2,13 @@
 
 set -exv
 
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+echo "$(cat kubectl.sha256)  ./kubectl" | sha256sum --check
+chmod +x kubectl
+# mv kubectl /container_workspace/bin
+# export PATH="/container_workspace/bin:$PATH"
+
 # Install krew for kubectl
 (
   set -x; cd "$(mktemp -d)" &&
