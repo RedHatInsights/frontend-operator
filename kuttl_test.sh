0c2479c0479def663ac0df9551ebcc782eb7445c
Matt Knop <mknop@redhat.com>
2024-10-29 10:05:24 -0600

    adding kubebuilder dir

--- kuttl_test.sh
+++ kuttl_test.sh
@@ -2,6 +2,10 @@
 
 set -exv
 
+mkdir -p /container_workspace/bin
+
+export KUBEBUILDER_ASSETS=/container_workspace/testbin/bin
+
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
 echo "$(cat kubectl.sha256)  ./kubectl" | sha256sum --check
