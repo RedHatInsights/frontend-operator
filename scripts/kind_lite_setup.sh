dd5bdf16e5e58a40d117e5455ba391c189996abf
Darth Tweed <btweed@redhat.com>
2025-07-07 18:41:14 -0500

    WIP

--- scripts/kind_lite_setup.sh
+++ scripts/kind_lite_setup.sh
@@ -1,9 +1,10 @@
-#!/bin/bash
+PR_IMAGE=quay.io\/cloudservices\/frontend-operator:sc-20250630-7c080ae#!/bin/bash
 # Configures a local kind cluster for testing FEO e2e.
 # Creates the cluster, installs required dependencies, installs Clowder, installs FEO
 
 set -e
 
+PR_IMAGE=quay.io\/cloudservices\/frontend-operator:sc-20250630-7c080ae
 CERT_MGR_VERSION='v1.18.2'
 
 if command -v "kind" >/dev/null 2>&1; then
@@ -39,7 +40,6 @@ kind create cluster
 kubectl config set-context kind-kind
 
 echo "Installing cert manager with kubectl"
-# TODO: Make the version configurable?
 ${KUBECTL_CMD} apply -f "https://github.com/cert-manager/cert-manager/releases/download/${CERT_MGR_VERSION}/cert-manager.yaml"
 
 # Wait for the cert manager api to be available before proceeding
@@ -51,14 +51,19 @@ done
 echo "Creating the boot namespace"
 ${KUBECTL_CMD} create namespace boot
 
-echo "Podman build and save the image, then load into cluster (local testing only)"
+# echo "Podman build and save the image, then load into cluster (local testing only)"
 # See https://github.com/kubernetes-sigs/kind/issues/2027 for more info on why this is needed
-# podman save
-# kind load image-archive
+# podman build -f Dockerfile -t controller:latest
+# podman image save localhost/controller -o image.tar
+# kind load image-archive image.tar
 
+#echo "Injecting PR_IMAGE ${PR_IMAGE} into the manifest"
+#cat manifest.yaml | sed -e "s/image\: controller\:latest/image\: ${PR_IMAGE}cat manifest.yaml | sed -e "s/image\: controller\:latest/image\: ${PR_IMAGE}/" > /tmp/manifest.tmp/" > /tmp/manifest.tmp
+
+# TODO: Define an overlay with the PR_IMAGE, then apply it with kustomize?
 
 echo "Applying FEO manifest"
-${KUBECTL_CMD} apply -f manifest.yaml
+${KUBECTL_CMD} apply -f /tmp/manifest.tmp
 
 echo "If the FEO is ready, this should show 1/1"
 ${KUBECTL_CMD}  get pods -n frontend-operator-system
